5ed28414e67d0c82c9cdeaa769eb02dadff6d2d1
tarak996 <tarak996/>
2020-06-21 10:18:21 +0530

    google ui

---
+++ gui/src/Logo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+//mport App from './App'; 
+//console.logo(logo)
+
+
+  function Logo() {
+    return( <div className="logo">
+       <h1> Google </h1>
+       </div>
+    );
+    
+  }
+    
+    
+
+export default Logo;
